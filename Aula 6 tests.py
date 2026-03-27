b26a4242f48561c8b654ca8a28bf566e7519106d
felipeB92 <felipe_B92@hotmail.com>
2026-03-27 18:05:37 -0300

    adicionados exercicios do 105 ao 112 + modulos e pacotes

--- Aula 6 tests.py
+++ Aula 6 tests.py
@@ -1,7 +1,10 @@
-n1 = int (input('digite um numero: '))
-n2 = int (input('digite mais um numero:'))
-soma = n1+n2
-#print('a soma de', n1,'e',n2,'é',soma)
-print ('a soma {} e {} é igual a {}'.format(n1,n2,soma))
+while True:
+    n = input('Digite o preço: R$').replace(',','.')
+    c = (n.replace(".",""))
+    if c.isnumeric():
+        b = float(n)
+        break
+    else:
+        print(f'\033[1;31m {n} é um valor invalido\033[m')
 
-
+print(b)
