75af9d9d475aa03cefa2c626a9efea1ecf0712e5
felipeB92 <felipe_B92@hotmail.com>
2026-04-16 20:16:47 -0300

    Ferramenta de automação de criaçao de personagem para Openbor

--- Ferramentas/Geração linhas de codigo.py
+++ Ferramentas/Geração linhas de codigo.py
@@ -1,6 +1,12 @@
-q = int(input('Quantidade de linhas'))
-local = input('Local dos aquivos')
-action = input('Nome dos aquivos')
+while True:
+    q = int(input('Quantidade de linhas: '))
+    local = input('Local dos aquivos: ')
+    action = input('Nome dos aquivos: ')
 
-for c in range(0,q):
-    print(f'    {local}{action}{c:02}.gif')
+    for c in range(0,q):
+        print(f'    {local}{action}{c:02}.gif')
+
+    print()
+    Q =input('quer gerar outro? [S/N]').upper()
+    if Q == 'N':
+        break
